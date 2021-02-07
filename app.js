cfd62a480fabe7a41189233401978d0a69cd81c2
Florian <mesagouiffes@gmail.com>
2021-02-07 10:24:18 +0100

    create develop and start recipes routes

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const recipesRoutes = require('./routes/recipes.routes');
+
+require("dotenv").config();
+const corsConfig = { credentials: true, origin: process.env.FRONT_ENDPOINT };
+
+app.use(express.json());
+app.use(cors(corsConfig));
+
+mongoose.connect(
+  process.env.DATABASE_LINK,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  () => {
+    console.log("✅ Database connected");
+  }
+);
+
+app.listen(process.env.PORT, () => {
+  console.log("✅ Server started");
+});
+
+app.use('/api', recipesRoutes);
+
+module.exports = app;
