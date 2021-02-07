84efb4b80a96090141f4767349f6f3a8d277a7de
Florian <mesagouiffes@gmail.com>
2021-02-07 16:34:24 +0100

    create recipes routes

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,8 +1,21 @@
 const { Router } = require('express');
 const router = Router();
 
+const {
+  getRecipes,
+  getRecipe,
+  insertRecipe,
+  updateRecipe,
+  deleteRecipe
+} = require('../controllers/recipe.controller');
+
 const BASE_NAME = "recipes";
 
-router.get(`/${BASE_NAME}`, (req, res) => console.log(`/${BASE_NAME} GET`))
+router.get(`/${BASE_NAME}`, getRecipes);
+router.get(`/${BASE_NAME}/:recipeId`, getRecipe);
+router.post(`/${BASE_NAME}`, insertRecipe);
+router.put(`/${BASE_NAME}/:recipeId`, updateRecipe);
+router.delete(`/${BASE_NAME}/:recipeId`, deleteRecipe);
+
 
 module.exports = router;
