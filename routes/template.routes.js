eb387fc6fd4e3a4213d904ffd83b055a163e9870
Florian <mesagouiffes@gmail.com>
2021-02-07 10:28:39 +0100

    create route template

---
+++ routes/template.routes.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const router = Router();
+
+const BASE_NAME = "recipes";
+
+router.get(`/${BASE_NAME}`, (req, res) => console.log(`/${BASE_NAME} GET`))
+
+module.exports = router;
